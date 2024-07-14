6240b254e36e155c1cd13230ccfc00730a923611
yoshihiro okuda <yoshi.private.acc@gmail.com>
2024-07-14 13:20:20 +0900

    replace

--- scripts/ci/update-version.ts
+++ scripts/ci/update-version.ts
@@ -1,4 +1,5 @@
 #!/usr/bin/env zx
+import { isNumber } from 'my-gadgetry/type-check'
 import { $ } from 'zx'
 
 const VERSION_MAP = {
@@ -12,20 +13,6 @@ const PRE_VERSION = {
   prefix: '$1',
   num: '$2',
 }
-/* eslint-disable @typescript-eslint/no-explicit-any */
-const getValueType = (value: any) => Object.prototype.toString.call(value).slice(8, -1)
-const isNumber = (value: any) => getValueType(value) === 'Number' && !Number.isNaN(value)
-const isString = (value: any) => getValueType(value) === 'String'
-const isNumberAllowString = (value: any) => {
-  if (isNumber(value)) {
-    return value - value === 0
-  }
-  if (isString(value) && value.trim() !== '') {
-    return Number.isFinite ? Number.isFinite(+value) : isFinite(+value)
-  }
-  return false
-}
-/* eslint-enable @typescript-eslint/no-explicit-any */
 
 const run = async () => {
   const currentVersion = (await $`jq -r .version package.json`).stdout.trim()
@@ -34,7 +21,7 @@ const run = async () => {
   const preVersionStr = versionMap.pre.replace(/^(\w+\.?)?(\w+)/, JSON.stringify(PRE_VERSION))
   if (preVersionStr) {
     const preVersionMap = JSON.parse(preVersionStr)
-    if (isNumberAllowString(preVersionMap.num)) {
+    if (isNumber(preVersionMap.num, true)) {
       versionMap.pre = `${preVersionMap.prefix}${parseInt(preVersionMap.num) + 1}`
     } else {
       versionMap.patch = parseInt(versionMap.patch) + 1
